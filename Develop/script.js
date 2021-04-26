e27caffe11b20faa6031e9d27059bb25b9a758ed
MRAx09 <martin.rae95@gmail.com>
2021-04-25 21:42:59 -0700

    code

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,29 @@
+var formEl = $('#cal-form');
+var listEl = $('#event-list');
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    var calEvent = $('input[name="calendar-input"]').val();
+
+    if (!calEvent) {
+        console.log('No events added to calendar!');
+        return;
+    }
+
+    var listeItemEl = $(
+        '<li class="flex-row justify-space-between align-center p-2 bg-light text-dark">'
+    );
+    listeItemEl.text(calEvent);
+
+    // add delete button to remove shopping list item
+    listeItemEl.append(
+        '<button class="saveBtn">Remove</button>'
+    )
+
+    listEl.append(listEl);
+}
+
 // TODO: 4. What is the current time in the format: hours:minutes:seconds
 var time = moment().format("hh:mm:ss");
 $("#4a").text(time);
